ffde6d5b5e13ffc2628057b94008a8420e393804
Shahriar220 <shahriar.uchchash@gmail.com>
2020-10-12 13:43:52 +0600

    if user is in the bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,19 @@
+//const message = require("../../server/utils/message");
 var socket = io();
+
+function scrollToBottom() {
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child')
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
 
 socket.on('connect', function() {
     console.log('Connected to server');
@@ -17,6 +32,7 @@ socket.on('newMessage', function(message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html)
+    scrollToBottom();
 
 });
 
@@ -30,6 +46,7 @@ socket.on('newLocationMessage', function(message) {
         createdAt: formattedTime
     })
     jQuery('#messages').append(html);
+    scrollToBottom();
 
 })
 
