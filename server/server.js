dcaec15ac5982fae832e01f006172269e23e918b
Shahriar220 <shahriar.uchchash@gmail.com>
2020-10-12 23:35:28 +0600

    sending location using the correct users name

--- server/server.js
+++ server/server.js
@@ -38,14 +38,19 @@ io.on('connection', (socket) => {
     })
 
     socket.on('createMessage', (message, callback) => {
-        console.log('create message', message)
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        var user = users.getUser(socket.id);
+        if (user && isRealString(message.text)) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
         callback();
 
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+        var user = users.getUser(socket.id);
+        if (user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude))
+        } else {}
     })
 
     socket.on('disconnect', () => {
@@ -60,6 +65,6 @@ io.on('connection', (socket) => {
 
 
 
-server.listen(3000, (req, res) => {
+server.listen(port, (req, res) => {
     console.log(`Listening to port ${port }`);
 })
